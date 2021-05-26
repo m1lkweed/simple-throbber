552f5c0eb17744125a19cf483afe924a9be1572e
m1lkweed <m1lkweed@users.noreply.github.com>
2021-05-26 01:18:27 -0400

    Update example.c

--- example.c
+++ example.c
@@ -4,12 +4,12 @@
 
 #include "throbber.h"
 
-#define MAX_THROBBERS 20
+#define MAX_THROBBERS 40
 #define RED "\x1b[31m"
 #define GREEN "\x1b[32m"
 
 int get_pos(int *y, int *x){
-	char buf[30] = {0};
+	char buf[30]={0};
 	int ret, i, pow;
 	char ch;
 	*y = 0;
@@ -20,7 +20,7 @@ int get_pos(int *y, int *x){
 	term.c_lflag &= ~(ICANON|ECHO);
 	tcsetattr(0, TCSANOW, &term);
 	write(1, "\x1b[6n", 4);
-	for(i = 0, ch = 0; ch != 'R'; ++i){
+	for( i = 0, ch = 0; ch != 'R'; ++i){
 		ret = read(0, &ch, 1);
 		if(!ret){
 			tcsetattr(0, TCSANOW, &restore);
@@ -51,7 +51,7 @@ int main(){
 	throbbers[0] = start_throbber(x, y, RED);
 	size_t i;
 	for(i = 1; i < MAX_THROBBERS; ++i){
-		fprintf(stdout, "Throbber %d: ", i);
+		fprintf(stdout, "Throbber %zu: ", i);
 		fflush(stdout);
 		get_pos(&x, &y);
 		puts("");
